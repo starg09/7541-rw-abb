02cb56f39460d0b46c2be26b2c0e4dc4ed699b06
Guillermo Bianchi <ultraguille2006@gmail.com>
2015-10-17 17:44:37 -0300

    Terminada documentación de abb.h

--- abb.h
+++ abb.h
@@ -2,9 +2,9 @@
 #define ABB_H
 
 
-// 
-// ÁRBOL BINARIO DE BÚSQUEDA
-// 
+////////////////////////////////////
+///  ÁRBOL BINARIO DE BÚSQUEDA  ///
+//////////////////////////////////
 
 
 // Definición de la estructura del ABB
@@ -50,25 +50,30 @@ void *abb_obtener(const abb_t *arbol, const char *clave);
 bool abb_pertenece(const abb_t *arbol, const char *clave);
 
 // Devuelve la cantidad de nodos en el ABB.
+// Pre: El ABB fue creado.
 size_t abb_cantidad(abb_t *arbol);
 
-// Destruye
+// Destruye el ABB, y sus datos de haberse proporcionado una función destructora al crear este.
+// Pre: El ABB fue creado.
+// Post: El ABB fue destruido completamente.
 void abb_destruir(abb_t *arbol);
 
 
 
-// 
-// ITERADOR INTERNO "INORDEN" DEL ÁRBOL BINARIO DE BÚSQUEDA
-// 
+///////////////////////////////////////////////////////////////////
+///  ITERADOR INTERNO "INORDEN" DEL ÁRBOL BINARIO DE BÚSQUEDA  ///
+/////////////////////////////////////////////////////////////////
 
-// 
+// Itera por el ABB mediante el orden "inorden", aplicando la función "visitar" a cada nodo.
+// Pre: El ABB fue creado.
+// Post: EL ABB fue recorrido. Se aplicó la función "visitar" a cada nodo.
 void abb_in_order(abb_t *arbol, bool (*visitar)(const char *, void *, void *), void *extra);
 
 
 
-// 
-//  ITERADOR EXTERNO "INORDEN" DEL ÁRBOL BINARIO DE BÚSQUEDA
-// 
+///////////////////////////////////////////////////////////////////
+///  ITERADOR EXTERNO "INORDEN" DEL ÁRBOL BINARIO DE BÚSQUEDA  ///
+/////////////////////////////////////////////////////////////////
 
 
 // Definición de la estructura del iterador externo del ABB
@@ -78,19 +83,28 @@ typedef struct abb_iter abb_iter_t;
 
 // Definición de las funciones del iterador externo de tipo "inorder" del ABB.
 
-// 
+// Crea un iterador de tipo "inorder" para el ABB provisto como parámetro.
+// Pre: El ABB fue creado. No va a ser modificado mientras exista el iterador.
+// Post: Se creó un iterador de tipo "inorder".
 abb_iter_t *abb_iter_in_crear(const abb_t *arbol);
 
-// 
+// Hace al iterador "inorder" avanzar. Devuelve false en caso de error.
+// Pre: el iterador "inorder" fue creado.
+// Post: devuelve true si avanzó con éxito, o false en caso de error.
 bool abb_iter_in_avanzar(abb_iter_t *iter);
 
-// 
+// Devuelve la clave del nodo en el que se encuentra actualmente el iterador "inorder".
+// Este string no se puede modificar, solo leer.
+// Pre: El iterador "inorder" fue creado.
+// Post: Se devuelve el puntero al string, o NULL en caso de error (por ej.: al final).
 const char *abb_iter_in_ver_actual(const abb_iter_t *iter);
 
-// 
+// Pre: El iterador "inorder" fue creado.
+// Post: Devuelve true o false según si el iterador "inorder" ya recorrió todo el ABB.
 bool abb_iter_in_al_final(const abb_iter_t *iter);
 
-// 
+// Destruye el iterador "inorder".
+// Pre: El iterador "inorder" fue creado.
 void abb_iter_in_destruir(abb_iter_t* iter);
 
 
